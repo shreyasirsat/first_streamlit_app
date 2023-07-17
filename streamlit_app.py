a78aa67a6e1fc1e6d29a6e75cb1b7dc96f7bdbdf
shreyasirsat <79854714+shreyasirsat@users.noreply.github.com>
2023-07-17 22:18:46 +0530

    Update streamlit_app.py
    Added space

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ streamlit.title('Breakfast Menu')
 
 streamlit.text('🥣 Omega3 and Bluberry oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
-streamlit.text('🐔Hard-bolied Free Ranged Eggs')
-streamlit.text('🥑Avacado')
+streamlit.text('🐔 Hard-bolied Free Ranged Eggs')
+streamlit.text('🥑 Avacado')
