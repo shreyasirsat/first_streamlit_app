36e42f363d43f763963145fe08f6a356357b58ee
shreyasirsat <79854714+shreyasirsat@users.noreply.github.com>
2023-07-17 22:15:33 +0530

    Update streamlit_app.py
    Added 4 new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.title('Breakfast Menu')
+
+streamlit.text('Omega3 and Bluberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamit.text('Hard-bolied Free Ranged Eggs')
